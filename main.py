7f84fa07b4e18f25b124960f4334222bd4ce713d
lozog <liam.ozog@gmail.com>
2017-07-11 20:56:56 -0400

    fix: wallpaper is no longer all black after logging out and in again

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ wallpaper = r"D:\projects\py-wallpaper-analog\img\mountain.jpg"
 
 # localtime = time.localtime(time.time())
 # wkd = localtime[6]
-ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, wallpaper, 0)
+ctypes.windll.user32.SystemParametersInfoW(SPI_SETDESKWALLPAPER, 0, wallpaper, 3)
